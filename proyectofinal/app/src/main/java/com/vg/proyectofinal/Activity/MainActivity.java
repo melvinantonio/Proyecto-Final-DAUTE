0df879c9764205f5b6487d8668f38bbcef2a075c
Kevin Torres <101214932+superkjr@users.noreply.github.com>
2022-11-14 01:25:44 -0600

    primera parte completada
    vistas completadas

---
+++ proyectofinal/app/src/main/java/com/vg/proyectofinal/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.vg.proyectofinal.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vg.proyectofinal.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
